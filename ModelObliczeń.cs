2237af0fe7d5b12e101c8853596326a5fcc4aa84
Paweł Bytner <pawel.m.bytner@gmail.com>
2022-05-29 15:56:19 +0200

    Dodaj pliki projektów.

---
+++ ModelObliczeń.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_ININ4_PR2._2_z3
+{
+    class ModelObliczeń : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string BuforIO { get; set; } = "0";
+        public string BuforDziałania { get; set; } = "Bufor działania";
+
+    }
+}
